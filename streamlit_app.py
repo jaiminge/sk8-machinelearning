a3916f65746e2640e2bec5cc53208182eb23c91a
jaiminge <jaiminge@gmail.com>
2025-07-19 12:29:40 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,10 @@
 import streamlit as st
+import pandas as pd
 
 st.title('🎈 ML app')
 
 st.info('This app builds ML models')
+
+df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+
+df
